465ca0d0ac842bdb5640694ee45e9cb7ddb0e4db
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-08-09 09:05:33 +0200

    chore:ex-oop

--- src/public/index.php
+++ src/public/index.php
@@ -40,7 +40,35 @@ echo $cocaCola->getInfo() . '<br>';
 
 <?php 
 // EXERCICE OOP 2
+class beer extends beverage{
+    public $name;
+    public $alcoholPercentage;
 
-
-
+    /**
+     * @param mixed $name
+     * @param mixed $alcoholPercentage
+     * @param mixed $color
+     * @param mixed $price
+     */
+    public function __construct($name, $alcoholPercentage, $color, $price){
+        parent::__construct($color, $price);
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+    }
+    /**
+     * @return [type]
+     */
+    function getAlcoholPercentage(){
+        return 'This is the alcohol percentage: ' . $this->alcoholPercentage . '.';
+    }
+}
+$duvel = new beer('duvel', 8.5, 'blond', 3.5);
+    echo $duvel->getAlcoholPercentage() . '<br>';
+    echo $duvel->getAlcoholPercentage();
+    echo "<br>";
+    echo $duvel->color;
+    echo "<br>";
+    echo $duvel->getInfo();
+    $beverage = new Beverage("red", 2.0);
+echo $beverage->getAlcoholPercentage();
 ?>
